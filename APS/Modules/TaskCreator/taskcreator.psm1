c05d701005771ba171e7c0a3ac0e5bdb7d49aa25
akotu235 <akotu@protonmail.com>
2022-09-07 14:47:27 +0200

    Removed digital signatures

--- APS/Modules/TaskCreator/taskcreator.psm1
+++ APS/Modules/TaskCreator/taskcreator.psm1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .SYNOPSIS
 Creates a new task.
 .DESCRIPTION
@@ -23,7 +23,7 @@ function New-APSTask {
         [string]$Command,
         [System.DateTime]$StartTime = $((Get-Date).AddSeconds(3)),
         [string]$TaskName = "APS Task($($Command.TrimStart('"& {').Split(" ")[0]))",
-        [ValidateSet(“Normal","Minimized","Maximized","Hidden")]
+        [ValidateSet(�Normal","Minimized","Maximized","Hidden")]
         [string]$WindowStyle = "Hidden",
         [switch]$Save
     )
@@ -51,36 +51,3 @@ function New-APSTask {
         }
     }
 }
-
-# SIG # Begin signature block
-# MIIFeQYJKoZIhvcNAQcCoIIFajCCBWYCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU0cOhOaCkkZGZpkyWM8c7B0cL
-# EVygggMQMIIDDDCCAfSgAwIBAgIQfziWHbCKBoRNGa23h81cKTANBgkqhkiG9w0B
-# AQsFADAeMRwwGgYDVQQDDBNQb3dlclNoZWxsIGFrb3R1IENBMB4XDTIyMDIwMTEz
-# MDExMloXDTI3MDIwMTEzMTExM1owHjEcMBoGA1UEAwwTUG93ZXJTaGVsbCBha290
-# dSBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAJ5Jah2xqCyY33yT
-# xhz7JjIQofP86RYwn5arpiQfKz87xvKTzOcVm9Pf3jrpKkcUnGL7PKqGDAX6HL5r
-# GQ7/2RPlnH7cSYIM9vYYmR7vgUUgQACsYVOO5UcrlDT9ga387gd7YInmSn/icot3
-# b2gvCf1Ok3OT05d8Vu4PzzYXNRvc6pIgnQ++ENakvB6LLSoso3OuZZoFhHpufD0/
-# 8ac21gw9ZeweFtQzy8BAkMbPCSSymiYduLPF4XEb1vo2w3fHDl/LYCfrJWOHTELS
-# IjpRLJQYbJnewBZ1x6jXRB0dTbUrO3C5UPoKXYPMIMi5Slvk1XPDHeXLOXAb4ZTO
-# EHV325kCAwEAAaNGMEQwDgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUF
-# BwMDMB0GA1UdDgQWBBR1hk5NfI7NaI/MFxkS4z6wB5uaszANBgkqhkiG9w0BAQsF
-# AAOCAQEABoUXCtmgDOiK6QjrzONCSE+7NbYrwzPonxGY0PNvmxf5Y6CcCK0Nga8v
-# ImAZM9cXAGOUZE0wZQUODHW4OxbW1kgzS4OvOQZZUeSPNG7OLxttYkF5+5Pfs8RY
-# AxkI0XYP3JId4Fx5E8ByMGx7wOpyVcLOCU+DEpEf21tHa4xQ5RGeKTcE7hRROLpg
-# g50DeoiSAeAmAH2K2l2uCPb+fP+MeEFH9THGPYJbWozU9Zq90Az3HCEn2dkPXKof
-# ZfBOJt3/WwSWGtYZqf0cAooTcKlO1TrreAmh4uuslfM7F579xKqX8ou1JzRQ2n/M
-# WRajsdVGAXYebpyYbjiGjNKoGzWS8DGCAdMwggHPAgEBMDIwHjEcMBoGA1UEAwwT
-# UG93ZXJTaGVsbCBha290dSBDQQIQfziWHbCKBoRNGa23h81cKTAJBgUrDgMCGgUA
-# oHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYB
-# BAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0B
-# CQQxFgQUWK7MIDJsfUVOIgkP1z41IGswaX0wDQYJKoZIhvcNAQEBBQAEggEAfgeC
-# /k+uVp2fhEYSmb7q62UEfrc3nEgKJLXdxlupKxDwvEnbUhIkqNe1sbRB3JXpdRx2
-# nad/AzZmCy25l+hY3CrJLa7/RzkFm3RdgTwW6IpRzg756c8DcQrQvAOEr4wGcM7M
-# R/Igxp4mFkUyr1ZLyaHUlRfRUJ3/+ehDB7bagopv2oJIsFK8gImkMtaecHz4OxqT
-# 3imcPdQdkLB5NEklnq0OL0FIT6dPeCNiifQZOip2DPjVZKBHA5aVuJy5teRXjaEd
-# j8Va5wOfOOyHHT6c9Ou5Tni1kJyZnWw3fwUCrCeeWmh3OokAV58OSFdDy3GDordx
-# FwcXY5PapU3r8ElJvA==
-# SIG # End signature block
--- APS/Modules/Weather/weather.psm1
+++ APS/Modules/Weather/weather.psm1
@@ -1,4 +1,4 @@
-ï»¿<#
+<#
 .SYNOPSIS
 Shows the weather.
 .DESCRIPTION
@@ -33,14 +33,14 @@ function Get-Weather{
         [string]$City,
         [switch]$Detailed,
         [switch]$Data,
-        [ValidateSet(â€œstandardâ€,â€metricâ€,â€imperialâ€)]
+        [ValidateSet(“standard”,”metric”,”imperial”)]
         [string]$Units,
         [switch]$RestoreDefaultConfig,
         [string]$SetDefaultCity,
         [string]$SetAppId,
-        [ValidateSet(â€œstandardâ€,â€metricâ€,â€imperialâ€)]
+        [ValidateSet(“standard”,”metric”,”imperial”)]
         [string]$SetDefaultUnits  ,
-        [ValidateSet(â€œenableâ€,â€disableâ€)]
+        [ValidateSet(“enable”,”disable”)]
         [string]$Geolocalization
     )
     if($RestoreDefaultConfig){
@@ -126,7 +126,7 @@ function Get-Weather{
                 Pressure="$($WeatherData.main.pressure) hPa";
                 "Cloud cover"="$($WeatherData.clouds.all) %";
                 Wind="$($WeatherData.wind.speed) $(Get-UnitSymbol $Units "V")";
-                Direction="$($WeatherData.wind.deg) Â°";
+                Direction="$($WeatherData.wind.deg) °";
                 Visibility="$($WeatherData.visibility) m";
             }
             return $WeatherObj
@@ -147,25 +147,25 @@ function Get-Weather{
 function Get-UnitSymbol{
     param(
         [Parameter(Mandatory=$true)]
-        [ValidateSet(â€œstandardâ€,â€metricâ€,â€imperialâ€)]
+        [ValidateSet(“standard”,”metric”,”imperial”)]
         [string]$Units,
         [Parameter(Mandatory=$true)]
-        [ValidateSet(â€œTâ€,â€Vâ€)]
+        [ValidateSet(“T”,”V”)]
         [string]$Size
     )
     if($Size -like "T"){
         if($Units -like "metric"){
-            return "Â°C"
+            return "°C"
         }
-        elseif($Units -like â€imperialâ€){
-            return "Â°F"
+        elseif($Units -like ”imperial”){
+            return "°F"
         }
-        elseif($Units -like â€standardâ€){
+        elseif($Units -like ”standard”){
             return "K"
         }
     }
     elseif($Size -like "V"){
-        if($Units -like â€imperialâ€){
+        if($Units -like ”imperial”){
             return "mph"
         }
         else{
@@ -174,36 +174,3 @@ function Get-UnitSymbol{
     }
 }
 Set-Alias "pogoda" Get-Weather
-
-# SIG # Begin signature block
-# MIIFeQYJKoZIhvcNAQcCoIIFajCCBWYCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUMy8NReSiXEbfwxvx/c36ToWY
-# sfWgggMQMIIDDDCCAfSgAwIBAgIQfziWHbCKBoRNGa23h81cKTANBgkqhkiG9w0B
-# AQsFADAeMRwwGgYDVQQDDBNQb3dlclNoZWxsIGFrb3R1IENBMB4XDTIyMDIwMTEz
-# MDExMloXDTI3MDIwMTEzMTExM1owHjEcMBoGA1UEAwwTUG93ZXJTaGVsbCBha290
-# dSBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAJ5Jah2xqCyY33yT
-# xhz7JjIQofP86RYwn5arpiQfKz87xvKTzOcVm9Pf3jrpKkcUnGL7PKqGDAX6HL5r
-# GQ7/2RPlnH7cSYIM9vYYmR7vgUUgQACsYVOO5UcrlDT9ga387gd7YInmSn/icot3
-# b2gvCf1Ok3OT05d8Vu4PzzYXNRvc6pIgnQ++ENakvB6LLSoso3OuZZoFhHpufD0/
-# 8ac21gw9ZeweFtQzy8BAkMbPCSSymiYduLPF4XEb1vo2w3fHDl/LYCfrJWOHTELS
-# IjpRLJQYbJnewBZ1x6jXRB0dTbUrO3C5UPoKXYPMIMi5Slvk1XPDHeXLOXAb4ZTO
-# EHV325kCAwEAAaNGMEQwDgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUF
-# BwMDMB0GA1UdDgQWBBR1hk5NfI7NaI/MFxkS4z6wB5uaszANBgkqhkiG9w0BAQsF
-# AAOCAQEABoUXCtmgDOiK6QjrzONCSE+7NbYrwzPonxGY0PNvmxf5Y6CcCK0Nga8v
-# ImAZM9cXAGOUZE0wZQUODHW4OxbW1kgzS4OvOQZZUeSPNG7OLxttYkF5+5Pfs8RY
-# AxkI0XYP3JId4Fx5E8ByMGx7wOpyVcLOCU+DEpEf21tHa4xQ5RGeKTcE7hRROLpg
-# g50DeoiSAeAmAH2K2l2uCPb+fP+MeEFH9THGPYJbWozU9Zq90Az3HCEn2dkPXKof
-# ZfBOJt3/WwSWGtYZqf0cAooTcKlO1TrreAmh4uuslfM7F579xKqX8ou1JzRQ2n/M
-# WRajsdVGAXYebpyYbjiGjNKoGzWS8DGCAdMwggHPAgEBMDIwHjEcMBoGA1UEAwwT
-# UG93ZXJTaGVsbCBha290dSBDQQIQfziWHbCKBoRNGa23h81cKTAJBgUrDgMCGgUA
-# oHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYB
-# BAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0B
-# CQQxFgQU/etRLT+lBhrdcXknmPanJjiI0W0wDQYJKoZIhvcNAQEBBQAEggEAXd3+
-# OSyvc5EbUvkr8xMEdDN5N5OHv4DXFBSfb0xkb6yFucz2bJ2poSNL+l6mbeHkpPpr
-# 8SFUAMMOwYJRDaQ9wEbD3PCXA/vsQ8oBQC6Ick8DZgllTQA6ro2c1bLhgAvJH2VD
-# FR7rXNi5rk3DelfBF6/PiybHQpTeo1yDsukki+3SbGWtWR6vR7IRyRyae3V0PeS8
-# /5i2oZM7HMlKUwa6/lCCpsVpnMmLDt68EXeFofzzzOHo9d3sutofs/m78Jizdrti
-# 7UViEgrqtdueIQ1/Oy8yD9u2pHMU0RRWl5fuJa8YPk6OjmQyyibJ+78k0PyjAxYv
-# QeG3gI7LiTrBrsuNGw==
-# SIG # End signature block
