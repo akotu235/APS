2ba0d6838289ed5c1817ae207220870a2f944ebf
akotu235 <akotu@protonmail.com>
2022-09-19 03:14:31 +0200

    Created Geolocalization module test

---
+++ Tests/Geolocalization.Tests.ps1
@@ -0,0 +1,19 @@
+BeforeAll{
+    $ModuleName = "Geolocalization"
+    Get-Module $ModuleName | Remove-Module
+    Import-Module "$PSScriptRoot\..\APS\Modules\$ModuleName"
+}
+Describe 'Get-Geolocation'{
+    Context 'no parameters' {
+        It 'contains a status field equal to "success"'{
+            $status = (Get-Geolocation | select status).status
+            $status  | Should -Be 'success'
+        }
+    }
+    Context 'with -City'{
+        It 'returns the name of the city'{
+            $city = Get-Geolocation -City
+            $city  | Should -BeOfType string
+        }
+    }
+}
