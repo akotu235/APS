f92dfbc4bf3c728ccdce5c1e988dc3e8ea13be13
akotu235 <akotu@protonmail.com>
2022-09-23 02:00:34 +0200

    Added reloading modules after update

--- APS/Modules/APSUpdater/apsupdater.psm1
+++ APS/Modules/APSUpdater/apsupdater.psm1
@@ -17,16 +17,25 @@ function Update-APS{
         [switch]$KeepPreviousVersion
     )
     $APS_Module = Get-Module APS
-    if(($APS_Module.Version) -lt (Get-APSCurrentVersion) -or $Force){
+    $currentVersionNumber = Get-APSCurrentVersion
+    $moduleBase = "$($APS_Module.ModuleBase)\..\$($currentVersionNumber.ToString())"
+    if(($APS_Module.Version) -lt $currentVersionNumber -or $Force){
         Update-Module APS -Force:$Force -Confirm:$false
         if(-not $KeepPreviousVersion){
-            Uninstall-Module APS -Force -RequiredVersion $APS_Module.Version -Confirm:$false -ErrorAction SilentlyContinue
+            Get-ChildItem "$moduleBase\.." | ForEach-Object {
+                if(($currentVersionNumber.ToString()) -notlike $_){
+                    Remove-Item ($_.FullName) -Recurse -Force
+                }
+            }
         }
-        Get-ChildItem -Path $($APS_Module.ModuleBase) -Recurse | Where-Object -Property Extension -Match ".psm?1" | ForEach-Object {
+        Get-ChildItem -Path $moduleBase -Recurse | Where-Object -Property Extension -Match ".psm?1" | ForEach-Object {
             $content = Get-Content -Path $_.FullName
             $content | ForEach-Object {$_.replace('`n','`r`n')}
             Set-Content -Path $_.FullName -Value $content
         }
+        Get-Module APS | Remove-Module
+        (Get-ChildItem "$moduleBase\Modules").Name | ForEach-Object {Remove-Module -Name $_ -ErrorAction SilentlyContinue }
+        Import-Module APS
     }
     else{
         Write-Host "APS is up to date" -ForegroundColor DarkGreen
@@ -54,11 +63,18 @@ function Get-APSCurrentVersion{
 }
 
 
+
+
+
+
+
+
+
 # SIG # Begin signature block
 # MIIIWAYJKoZIhvcNAQcCoIIISTCCCEUCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU7Wxq5WUupHZGoUk3jtL5GRjB
-# kgqgggT6MIIE9jCCAt6gAwIBAgIQYYPyfUBBC6pE/rAfOslXOzANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUP9CNjDBYRGjSnv/bWUOlP5lL
+# JQSgggT6MIIE9jCCAt6gAwIBAgIQYYPyfUBBC6pE/rAfOslXOzANBgkqhkiG9w0B
 # AQsFADATMREwDwYDVQQDDAhha290dSBDQTAeFw0yMjA5MjAxOTQ4MDFaFw0zMjA5
 # MjAxOTU4MDFaMBMxETAPBgNVBAMMCGFrb3R1IENBMIICIjANBgkqhkiG9w0BAQEF
 # AAOCAg8AMIICCgKCAgEAvGcae/FCZugTbghxO7Qv9wQKvRvp9/WvJyJci/SIsPr1
@@ -88,16 +104,16 @@ function Get-APSCurrentVersion{
 # ETAPBgNVBAMMCGFrb3R1IENBAhBhg/J9QEELqkT+sB86yVc7MAkGBSsOAwIaBQCg
 # eDAYBgorBgEEAYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEE
 # AYI3AgEEMBwGCisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJ
-# BDEWBBTV/dltms4YgOd3uBPWlZe0/kAp0jANBgkqhkiG9w0BAQEFAASCAgCRGfWL
-# e+I976Q7KFNwtCogy/6UlNbYhSomq9KZwv2M0K9mLnizyR4aLxU5w4/2zAwVom3/
-# 62spyMgn9PYIKYgF0E2KGzwVPtu/PQsIMZzUpIg+B1n84xA9m2AdagwXlhs6NJwd
-# +xNugjt8F6R0IlZLhLH+Db/YugVsai+gBswpwez4INpWufG5SL0fn0wIUOvsVyK8
-# Pwm9gFqKyFfsGiVLn683wNoT0U9Z5qZ8G5v4LHsQ23G1SC0/8a5XKP50XGV9UIj1
-# HnD7qvTlKIbiaNdCi5ifW7zF0PKzJbX9LG+MU8cqnxpHWcGciHai2Sxpaz8W3ibp
-# NdfAATzqGxCoPEFZBvgL2j8Ghn9bffQIbllPes4Gim2VlS/T3KkG2ioRF4yO4mA8
-# qdtNfYRxanRzN4oGTWslbjRTmn2eygGLiVBGrFRjKNCtK706r5ufpRUizldGNshZ
-# q8jo/2YT1TibRDWZV7YQBpb4KNcsafM/iueddUVEOGvV8yp2JqbaJALcdgm53GlE
-# x82TNxUuuLHRgAn5Nz7ls03mqjrmdk0O/l8DhHmNNEOhwiTGG+h87+KgbWfS29V0
-# Qa3/xSRVb8Ab2Uni92IhTUl5rRzXuJSITNyKZqAtyFgIf5YBdbF/hiWz+hwSCQPb
-# 13ov1fTXw3K8U/wD6sISnilGlfpFGaKLxAR9Tw==
+# BDEWBBQ16onyMVg3NnNs3LZhoyMiittXUTANBgkqhkiG9w0BAQEFAASCAgCsPmO0
+# rMCkw5CCVAkJDHp7KMZLEeNCh53PvUPFP9H+MPRuwrtYFj+sqPscurkomtUIUPmj
+# XwQNhH9X5qKW8J5YuiAtZnVrItEIFgKSQ+M+6NR+Wo9EtRDapuKVmESO4GrVl/Rl
+# T1pvDj8gZvm6zTT08Ju6E20/S3waYFxf+5gzMxZk6XXePByu8+QCk9f2ctMw5PyJ
+# rnHeAK11H37p0LsdkMNfBABkyWnvwagcLPbcLVU2g6b3VSU1+0620Y2GtVIo9RZJ
+# vX+yvSuqfR5Zov+DAWqkmJpWCnfgYR7Dd1b6ye8z0ynmJnOT2+DtJcBHTE16WTti
+# oAb+qN2hTGkPLCPxmy8cM5lrotYZPJuDiHuwr15a/ZSFIYcABetdXVNwYtC6+Yj8
+# BtPy/z3pfFEpuj1nUrB/1QeE/sD4+8mzJbm6+fjENhfTvQuYyGXwWhcXYcpeiEuA
+# DbLjrtylicfH73sj8Zn4BBj8UiKLgaivxn6PZ58eacNpueeHam3jy8i94P9shBhM
+# aE43aqv9wx/PAgmRI8DbYb1cgS/zA+d3r8anYAa3CNzEwyZSgdBvJnpwIaoy3L62
+# 7rJmIlIIPF8ukVVtDCY1chPMF7B63FDXab64Qs/8/u5AN/j/5HpCM3QlXhkp1PqK
+# Wb0zKUeim9BfLF5FX7FntHBHZL//nApr/aTLTw==
 # SIG # End signature block
